f80ee04fa35364080dab9d4c40471433c9dbd461
DanFar562 <alandfarfan@gmail.com>
2025-11-26 15:24:11 -0600

    cambios menú, indicadores y sprites

--- source/screens/menus/parallax_layer_2.gd
+++ source/screens/menus/parallax_layer_2.gd
@@ -1,7 +1,7 @@
 extends ParallaxLayer
 
 var dir = Vector2(1,0)
-var velocidad = 30 
+var velocidad = 45 
 
 func _process(delta):
 	motion_offset += dir * velocidad * delta
--- source/screens/menus/parallax_layer_3.gd
+++ source/screens/menus/parallax_layer_3.gd
@@ -1,8 +1,7 @@
 extends ParallaxLayer
 
-
 var dir = Vector2(1,0)
-var velocidad = 15 
+var velocidad = 30 
 
 func _process(delta):
 	motion_offset += dir * velocidad * delta
